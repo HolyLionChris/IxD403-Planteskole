1d5db5674ab65e3a1862758e2677bca0d8f06e4d
EliasIXDesign <69719887+EliasIXDesign@users.noreply.github.com>
2021-03-25 11:13:08 +0100

    Added ConsoleApp up to date

---
+++ ConsoleAppPlanteskole1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Planteskole.Domain.Models;
+using Planteskole.Domain.Services;
+using Planteskole.EntityFramework;
+using Planteskole.EntityFramework.Services;
+using System;
+
+namespace ConsoleAppPlanteskole1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IDataService<Plant> userService = new GenericDataService<Plant>(new PlanteskoleDbContextFactory());
+        }
+    }
+}
