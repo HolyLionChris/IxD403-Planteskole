85598d14720c30a09c1a6baaf186dc6341c00856
Draysh <shirvani7@hotmail.com>
2021-03-26 12:04:39 +0100

    MVVM

---
+++ Planteskole.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Planteskole.WPF.State.Navigators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planteskole.WPF.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public INavigator Navigator { get; set; } = new Navigator();
+    }
+}
