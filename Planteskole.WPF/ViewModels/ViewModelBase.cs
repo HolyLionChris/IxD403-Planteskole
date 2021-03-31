3482e6abba3767b45abb355277ae315d9fd72f09
Heino Christiansen <hchri19@student.aau.dk>
2021-03-31 11:45:27 +0200

    add more s**t

--- Planteskole.WPF/ViewModels/ViewModelBase.cs
+++ Planteskole.WPF/ViewModels/ViewModelBase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Planteskole.WPF.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Planteskole.WPF.ViewModels
 {
-    public class ViewModelBase
+    public class ViewModelBase : ObservableObject
     {
     }
 }
