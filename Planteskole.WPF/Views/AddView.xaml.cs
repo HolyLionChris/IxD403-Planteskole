fb308dc5ca5feaf48e172cbbf8d64f854619704a
Draysh <shirvani7@hotmail.com>
2021-04-29 13:14:28 +0200

    Visible plant thing on startup

--- Planteskole.WPF/Views/AddView.xaml.cs
+++ Planteskole.WPF/Views/AddView.xaml.cs
@@ -1,23 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Planteskole.WPF.Temporary;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Planteskole.Domain;
-using Planteskole.WPF.ViewModels;
-using Planteskole.WPF.Models;
-using Planteskole.WPF.Commands;
 using Planteskole.Domain.Models;
 
 namespace Planteskole.WPF.Views
