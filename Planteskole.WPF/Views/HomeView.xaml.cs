9d090413c8f56682815316394a08b764c1238a7a
Heino Christiansen <hchri19@student.aau.dk>
2021-04-30 14:54:21 +0200

    More styling

--- Planteskole.WPF/Views/HomeView.xaml.cs
+++ Planteskole.WPF/Views/HomeView.xaml.cs
@@ -27,8 +27,16 @@ namespace Planteskole.WPF.Views
             //PlantViewSource = (CollectionViewSource)FindResource(nameof(PlantViewSource));
             DataContext = new ViewModels.HomeViewModel();
 
-
         }
-        
+        private void DataGridCell_Selected(object sender, RoutedEventArgs e)
+        {
+            // Lookup for the source to be DataGridCell
+            if (e.OriginalSource.GetType() == typeof(DataGridCell))
+            {
+                // Starts the Edit on the row;
+                DataGrid grd = (DataGrid)sender;
+                grd.BeginEdit(e);
+            }
+        }
     }
 }
